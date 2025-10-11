ee342c54cff3062c24d46510abcff33c2ae06ac1
ropi-design <h.sato.tokyozidousya@gmail.com>
2025-10-11 15:48:15 +0900

    一覧機能作成

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Livewire\Volt\Volt;
 Route::get('/', function () {
     return view('welcome');
 });
-
+// 一覧ページ
+Volt::route('/memos', 'memos.index')->name('memos.index');
 // 詳細ページ
 Volt::route('/memos/{memo}', 'memos.show')->name('memos.show');
